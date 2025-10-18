d925bd9371f25a5d2be77e98baf501d6f14625a5
nisaulucay <hayrunnisaulucaya@gmail.com>
2025-10-18 18:45:20 +0300

    commit

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Veri setini oku
 df = pd.read_csv("StudentsPerformance.csv")
 
-# Cinsiyete göre ortalama matematik puanı
+# cinsiyete göre ortalama matematik puanı
 ortalama_math = df.groupby('gender')['math score'].mean()
 
-# Bar grafiği
+# bar grafiği
 ortalama_math.plot(kind='bar', color=['pink', 'lightblue'])
 plt.title("Cinsiyete Göre Ortalama Matematik Puanı")
 plt.ylabel("Ortalama Puan")
 plt.xlabel("Cinsiyet")
 plt.show()
 
-# 2️⃣ Matematik Puanı Dağılımı (Histogram)
+# matematik puanı dağılımı (Histogram)
 plt.figure(figsize=(6,4))
 plt.hist(df['math score'], bins=10, color='lightgreen', edgecolor='black')
 plt.title("Matematik Puan Dağılımı")
